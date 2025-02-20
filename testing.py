f239c94d6f7a580bbe3155373ce0a2c73c209070
MateuszKielan <mateuszkielan38@gmail.com>
2025-02-20 16:06:32 +0100

    Managed to connect vocabulary recommender

---
+++ testing.py
@@ -0,0 +1,62 @@
+import requests
+import csv
+import json
+
+# Lov api url
+recommender_url = "https://lov.linkeddata.es/dataset/lov/api/v2/term/search"
+
+def get_csv_headers(file_path):
+    """
+    Function get_csv_header that opens a file and extracts headers from the csv for parsing into the vocabulary
+
+    Params:
+        file_path (str) : path of the file
+    Return:
+        headers (arr) : headers of the csv
+    """
+    with open(file_path, "r", encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file)
+        headers = next(reader)
+    return headers
+
+def get_recommendations(headers):
+    """
+    Function get_recommendations that receives headers and runs a get requests to the vocabulary api
+
+    Params:
+        headers (arr) : headers of the csv file
+    Return:
+        None
+
+    """
+    send = headers[0]
+
+    params = {
+        "q": send,
+        "category": "class", 
+        "page_size": 5
+    }
+
+    response = requests.get(recommender_url, params=params)
+
+    results = response.json()
+    display_results(results)
+
+def display_results(results):
+    """
+    Fucntion display_results that takes query results and displays them in a readable format
+    
+    Params:
+        results (dict) : query results converted to json
+    """
+    print(results['results'])
+#--------------------------------------------------------------
+# Main Function to run the request
+def main():
+    csv_file = "examples/cow_person_example.csv"
+    headers = get_csv_headers(csv_file)
+    recommendations = get_recommendations(headers)
+
+if __name__ == "__main__":
+    main()
+#--------------------------------------------------------------
