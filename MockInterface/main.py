31096da0b137cc1ce09256195ed1f4c92a3ae6e7
MateuszKielan <mateuszkielan38@gmail.com>
2025-03-05 20:28:39 +0100

    Starting Screen
    Done:
    - Created Screen manager for two screens
    - Added Switching the Screen on the press of "convert" button
    Next:
    - Create Basic Layout for the converter screen

--- MockInterface/main.py
+++ MockInterface/main.py
@@ -3,12 +3,13 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from plyer import filechooser
 from pathlib import Path
 #-----------------------------
 
-class StartingScreen(Widget):
+class StartingScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -20,9 +21,19 @@ class StartingScreen(Widget):
             file_path_name = str(Path(selection[0]).name)
             self.ids.file_path_label.text = file_path_name
 
+    def switch(self, item=None):
+        self.manager.current = "converter"
+
+class ConverterScreen(Screen):
+    pass
+
+
 class CowApp(App):
     def build(self):
-        return StartingScreen()
+        sm = ScreenManager()
+        sm.add_widget(StartingScreen(name="start"))
+        sm.add_widget(ConverterScreen(name="converter"))
+        return sm
     
 if __name__ == '__main__':
     CowApp().run()
