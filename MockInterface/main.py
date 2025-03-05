b38f384c521c1176507cd44075ba028bce37809c
MateuszKielan <mateuszkielan38@gmail.com>
2025-03-05 20:00:24 +0100

    Starting Page
    Done:
    - Added displaying the name of the selected file
    Next:
    - Add screen shifting on press

--- MockInterface/main.py
+++ MockInterface/main.py
@@ -5,6 +5,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
 from plyer import filechooser
+from pathlib import Path
 #-----------------------------
 
 class StartingScreen(Widget):
@@ -13,9 +14,11 @@ class StartingScreen(Widget):
 
     def select_file(self):
         filechooser.open_file(on_selection=self.select_store)
-
+        
     def select_store(self, selection):
-        print(selection)
+        if selection:
+            file_path_name = str(Path(selection[0]).name)
+            self.ids.file_path_label.text = file_path_name
 
 class CowApp(App):
     def build(self):
