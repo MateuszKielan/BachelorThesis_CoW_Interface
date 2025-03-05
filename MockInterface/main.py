e1dacfe96edf7d88d8463f402abc13dc15271d10
MateuszKielan <mateuszkielan38@gmail.com>
2025-03-05 17:34:20 +0100

    Starting Screen
    Done:
    - Added File selection Feature
    - Added storing of the file path on the computer
    Next:
    - Display the file name on the screen upon selection
    - Add "Convert" button to proceed to the next page

--- MockInterface/main.py
+++ MockInterface/main.py
@@ -4,14 +4,19 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
+from plyer import filechooser
 #-----------------------------
 
 class StartingScreen(Widget):
-    #def __init__(self, **kwargs):
-        #super().__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    #def select_file(self):
-    pass
+    def select_file(self):
+        filechooser.open_file(on_selection=self.select_store)
+
+    def select_store(self, selection):
+        print(selection)
+
 class CowApp(App):
     def build(self):
         return StartingScreen()
