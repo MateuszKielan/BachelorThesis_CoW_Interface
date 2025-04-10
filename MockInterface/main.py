39124be6b9a8e21bee64de9108dbd65cf2f1de4a
MateuszKielan <mateuszkielan38@gmail.com>
2025-04-10 22:27:24 +0200

    Recommendations page
    - Implemented Sampling recommendations

--- MockInterface/main.py
+++ MockInterface/main.py
@@ -13,7 +13,7 @@ from plyer import filechooser
 from pathlib import Path
 from screeninfo import get_monitors
 from kivy.core.window import Window
-from requests_t import get_csv_headers, get_recommendations
+from requests_t import get_csv_headers, get_recommendations, organize_results  # My implementation of single / homogenouse requests
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
 import csv
@@ -65,7 +65,15 @@ class DataPopup(FloatLayout):
         self.build_table(column_heads, row_data)
 
     def build_table(self, column_heads, row_data):
-        
+        """
+        Function build_table that builds the table for the whole dataset.
+
+        Params:
+            column_heads(arr): list of headers
+            row_data(arr): list of data row by row
+        """
+
+        # Define the table
         table = MDDataTable(
             column_data=column_heads,
             row_data=row_data,
@@ -75,6 +83,7 @@ class DataPopup(FloatLayout):
             rows_num=20
         )
 
+        # Clear the previous table on launch and add the new table to the widget
         self.ids.popup_data_container.clear_widgets()
         self.ids.popup_data_container.add_widget(table)
 
@@ -87,20 +96,68 @@ class DataPopup(FloatLayout):
                 break
             parent = parent.parent
     
-        
+class RecommendationPopup(FloatLayout):
+    def __init__(self, header, organized_data, list_titles, **kwargs):
+        super().__init__(**kwargs)
+        self.build_table(header, organized_data, list_titles)
+
+
+    def build_table(self, header, organized_data,list_titles):
+        """
+        Function build_table that builds the table for every header.
+
+        Params:
+            column_heads(arr): list of headers
+            row_data(arr): list of data row by row
+        """
+
+        # Define the table
+        table = MDDataTable(
+            column_data=list_titles,
+            row_data=organized_data,
+            size_hint=(0.98, 0.85),
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+            use_pagination=True,
+            rows_num=20
+        )
+
+        # Clear the previous table on launch and add the new table to the widget
+        self.ids.popup_recommendations.clear_widgets()
+        self.ids.popup_recommendations.add_widget(table)
+
+    def dismiss_popup(self):
+
+        parent = self.parent
+        while parent:
+            if isinstance(parent, Popup):
+                parent.dismiss()
+                break
+            parent = parent.parent
+    
 class ConverterScreen(Screen):
     def show_popup(self, column_heads, row_data):
         """
         Function show_popup that calls the DataPopup class to display the table 
+
+        Params:
+            column_heads(arr): list of headers
+            row_data(arr): list of data row by row
+
+        Those parameters are only needed to pass it further to the DataPopup screen
         """
+
+        # Pass the data to the popup
         show = DataPopup(column_heads, row_data)
 
+        # Initialize window
         popupWindow = Popup(title="CSV Data", content=show, size_hint=(1, 1))
 
         popupWindow.open()
     
-    def open_recommendations():
-        pass
+    def open_recommendations(self,header, data,list_titles):
+        show = RecommendationPopup(header,data,list_titles)
+        popupWindow = Popup(title=f'Matches for {header}', content=show,size_hint=(1,1))
+        popupWindow.open()
     
     def display_recommendation(self, file_path):
         """
@@ -110,13 +167,19 @@ class ConverterScreen(Screen):
             3. Displays the recommendations on the middle page.
         """
         headers = get_csv_headers(file_path)
-        size = 1
+        size = 20
 
         table = self.ids.vocab_recommender
+
+        all_results = {}
+        list_titles = [('prefixedName', dp(50)), ('vocabulary.prefix', dp(50)), ('uri',dp(50)), ('type',dp(50)), ('score',dp(50))]
+
         for header in headers:
             recommendations = get_recommendations(header, size)
-            table.add_widget(Label(text=f'{header}', bold=True, color=(0, 0, 0, 1)))
-            #table.add_widget(Label(text=f'{recommendations}', bold=True, color=(0,0,0,1)))
+            organized_data = organize_results(recommendations)
+            table.add_widget(Button(text=f'{header}', on_press=lambda x: self.open_recommendations(header, organized_data, list_titles), bold=True, color=(0, 0, 0, 1)))
+            all_results[header] = organized_data
+        
 
         # Load CSV data for table
         with open(file_path, newline='', encoding='utf-8') as csvfile:
@@ -130,7 +193,7 @@ class ConverterScreen(Screen):
         table_rows = rows[1:11] 
         row_data = rows [1:]
 
-        # Remove old tabless
+        # Remove old tables
         if hasattr(self, 'csv_table'):
             self.remove_widget(self.csv_table)
 
@@ -144,7 +207,7 @@ class ConverterScreen(Screen):
             rows_num = 10
         )
 
-        # Add table to left section
+        # Clear the previous table on launch and add the new table to the widget
         self.ids.csv_preview_container.clear_widgets()
         self.ids.csv_preview_container.add_widget(self.csv_table)
 
