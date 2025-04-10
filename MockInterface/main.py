fea23b7bf5764ec91b26e56a7005f1e8092a0308
MateuszKielan <mateuszkielan38@gmail.com>
2025-04-10 15:45:13 +0200

    Data Display Section
    - Displaying first 10 rows of the table
    - created a popup to display a full table

--- MockInterface/main.py
+++ MockInterface/main.py
@@ -1,16 +1,21 @@
 #------Imports Section-------
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 from kivy.lang import Builder
 from kivy.config import Config
 from plyer import filechooser
 from pathlib import Path
 from screeninfo import get_monitors
 from kivy.core.window import Window
-from MockInterface.requests_t import get_csv_headers, get_recommendations
+from requests_t import get_csv_headers, get_recommendations
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
+import csv
 #-----------------------------
 
 # Set the adaptive fullScreen mode
@@ -50,7 +55,20 @@ class StartingScreen(Screen):
         converter_screen.display_recommendation(self.selected_file)
         self.manager.current = "converter"
 
+class DataPopup(FloatLayout):
+    """
+    Class DataPopup that defines a popup page that displays full csv data table
+    """
+    
+        
 class ConverterScreen(Screen):
+    def show_popup(self):
+        show = DataPopup()
+
+        popupWindow = Popup(title="CSV Data", content=show, size_hint=(1, 1))
+
+        popupWindow.open()
+    
     def display_recommendation(self, file_path):
         """
         Function display_recommnedation:
@@ -63,12 +81,39 @@ class ConverterScreen(Screen):
 
         table = self.ids.vocab_recommender
         for header in headers:
-            recommendations = get_recommendations(headers, size)
+            recommendations = get_recommendations(header, size)
             table.add_widget(Label(text=f'{header}', bold=True, color=(0, 0, 0, 1)))
             #table.add_widget(Label(text=f'{recommendations}', bold=True, color=(0,0,0,1)))
 
+        # Load CSV data for table
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            rows = list(reader)
+            if not rows:
+                return
 
-class CowApp(App):
+        # Seperate headers and row data
+        column_heads = [(header, dp(25)) for header in rows[0]]
+        print(column_heads)
+        table_rows = rows[1:11] 
+
+        # Remove old tabless
+        if hasattr(self, 'csv_table'):
+            self.remove_widget(self.csv_table)
+
+        # Create MDDataTable
+        self.csv_table = MDDataTable(
+            column_data=column_heads,
+            row_data=table_rows,
+            size_hint=(0.90, 0.80),
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+        )
+
+        # Add table to left section
+        self.ids.csv_preview_container.clear_widgets()
+        self.ids.csv_preview_container.add_widget(self.csv_table)
+
+class CowApp(MDApp):
     def build(self):
         """
         Build app function that runs the Screen Manager.
