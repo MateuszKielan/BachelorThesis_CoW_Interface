d5120bbd1fdcc5008edcc0c191377c67ef9f5a89
Mateusz Kielan <71171185+MateuszKielan@users.noreply.github.com>
2025-06-23 18:40:32 +0200

    Update main.py

--- Interface/main.py
+++ Interface/main.py
@@ -1634,6 +1634,7 @@ class CowApp(MDApp):
         """
         Build app function that runs the Screen Manager.
         """
+        Builder.load_file("cow.kv")
         # Set the adaptive fullScreen mode
         Window.maximize()
         sm = ScreenManager()
